3c627da53820f47e8254f3233d245c3cf3c0b031
kubanychbek11 <kuba.seitkaziev@gmail.com>
2022-12-11 14:45:19 +0600

    hw2

--- Hero.py
+++ Hero.py
@@ -16,12 +16,69 @@ class SuperHero:
         return  f'{self.nickname, self.superpower, self.health_points}'
     def __int__(self):
         return len(self.catchphrase)
-Hero=SuperHero('Алишер', 'Клоун','деньги',666,'все хотят от меня шоу')
+Hero=SuperHero('Ben', 'Batman','деньги',200,'все хотят от меня шоу')
 Hero.realname()
 Hero.hpx2()
-Hero.__str__()
-Hero.__int__()
+
 print(Hero.realname())
 print(Hero.hpx2())
-print(Hero.__str__())
-print(Hero.__int__())
+
+class Earth(SuperHero):
+    sign = True
+    def __init__(self,name,nickname,superpower,health_points,catchphrase,damage=False,fly=False):
+        super().__init__(name,nickname,superpower,health_points,catchphrase)
+        self.damage = damage
+        self.fly = fly
+    def hpx2(self):
+        return f'{self.health_points ** 2}'
+    def fly_h(self):
+        self.fly=True
+
+    def phrase(self):
+        print('fly in the True_phrase')
+
+
+Earth_h = Earth('Bob', 'Stebel', 'laser', 100, 'babushka boi')
+Earth_h.hpx2()
+Earth_h.phrase()
+print(Earth_h.hpx2())
+
+
+class Air(SuperHero):
+    cape = True
+    def __init__(self,name,nickname,superpower,health_points,catchphrase,damage=False,fly=False):
+        SuperHero.__init__(self,name,nickname,superpower,health_points,catchphrase)
+        self.damage = damage
+        self.fly = fly
+    def hpx2(self):
+        return  f'{self.health_points **2}'
+    def fly_h(self):
+        self.fly=True
+
+    def phrase(self):
+         print('fly in the True_phrase')
+
+Air_h = Air('carl','invisy', 'invisible', 30, 'where am i')
+Air_h.hpx2()
+Air_h.phrase()
+print(Air_h.hpx2())
+
+
+class Villain(Air):
+    people = 'monster'
+    def __init__(self,name,nickname,superpower,health_points,catchphrase,damage=False,fly=False):
+        super().__init__(name,nickname,superpower,health_points,catchphrase,damage=False,fly=False)
+    def gen_x(selfs):...
+    def crit(self):
+        return f'{self.damage **2}'
+evil = Villain('Ivan','kuvalda','strength',300,'я бью 2 раза')
+evil.crit(Air_h.damage)
+
+
+
+
+
+
+
+
+
